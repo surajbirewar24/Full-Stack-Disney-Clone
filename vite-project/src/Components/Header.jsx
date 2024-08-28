4e21f270e0bada2431de2b2adc4a6dd653fa7fa6
surajb <surajbirewar001@gmail.com>
2024-08-29 00:14:48 +0530

    set a responsive headerItem for different screen size

--- vite-project/src/Components/Header.jsx
+++ vite-project/src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import logo from './../assets/Images/logo.png'
 import { HiHome,
   HiMagnifyingGlass,
@@ -10,6 +10,7 @@ import {HiPlus,HiDotsVertical} from "react-icons/hi";
 import HeaderItem from './HeaderItem';
 
 function Header() {
+  const [toggle,setToggle]=useState(false);
   const menu=[
     {
       name:'HOME',
@@ -40,10 +41,28 @@ function Header() {
     <div className='flex items-center justify-between'>
       <div className='flex gap-8 items-center'>
       <img src={logo} className='w-[80px] md:w-[115px] object-cover'/>
+      <div className='hidden md:flex gap-8'>
       {menu.map((item)=>(
         <HeaderItem  name={item.name} Icon={item.icon}/>
 
       ))}
+      </div>
+      <div className='flex md:hidden gap-5'>
+      {menu.map((item,index)=>index<3&&(
+        <HeaderItem  name={''} Icon={item.icon}/>
+
+      ))}
+      <div className='md:hidden' onClick={()=>setToggle(!toggle)}>
+        <HeaderItem name={''} Icon={HiDotsVertical}/>
+        {toggle? <div className='absolute mt-3 bg-[#121212] border-[1px] border-gray-700 p-3 px-5 py-4'>
+        {menu.map((item,index)=>index>2&&(
+        <HeaderItem  name={item.name} Icon={item.icon}/>
+
+      ))}
+        </div>:null}
+      </div>
+
+    </div>
     </div>
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3AGnMFzOLvv-du52tYrZXv32ChA-0yr0urA&s"
     className='w-[40px] rounded-full'/>
